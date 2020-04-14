96deb53f0158953180ff5e17a9f3b47370dd0b8d
Fabrice Domoison <39194172+Fabgraph@users.noreply.github.com>
2020-04-14 17:07:13 +0200

    commit03

--- 01-variables/variables.js
+++ 01-variables/variables.js
@@ -39,10 +39,221 @@ console.log(doubleQuote); // Affiche dans la console : Double quote ou guillemet
 
 // Backticks
 
-let addInfo = "Depuis ES6";
+let addInfo = "depuis ES6";
 let variable4 = `Les backticks sont apparus ${addInfo}`;
 // let variable5 = `Les backticks sont apparus ${addInfo}`;
+
+// Apparus sous ES6, les backtricks se caractérisent par les ``. Pour faire appel, dans une chaîne de caractères, une autre variable, il faut utiliser la syntaxe suivante : ${}
 
 document.write(variable4);
 // console.log(variable5)
 
+//---------CONCATENATION--------------
+
+let concatenation = "Les backticks sont apparus";
+
+console.log(concatenation + " " + addInfo); // Concaténation consiste à ajouter une chaîne de caractères à la suite d'une autre chaîne de caractères. Elle est caractérisée par le symbole +
+
+
+//--------------------
+//Les constantes
+
+document.write("<h1>Les constantes</h1>");
+
+document.write("<p>Une constante est un espace de stockage qui permet de stocker tout type de données (chaine de caractères, valeur numérique, booléan...). Mais dont la valeur initiale ne peut être modifiée</p>");
+
+let maVariableLet = "Première valeur";
+
+console.log(maVariableLet); // Affiche : Première valeur
+
+maVariableLet = "Deuxième valeur qui remplace la valeur initiale";
+
+console.log(maVariableLet); // Affiche : Deuxième valeur qui remplace la valeur initiale
+
+//-----------
+
+const maConstante = "Valeur initiale";
+
+console.log(maConstante); // Valeur initiale
+
+// maConstante = "Nouvelle valeur"; // Message d'erreur arrêt de l'exécution du script, on ne peut pas modifier la valeur d'une constante
+
+// console.log(maConstante); // Erreur
+
+//--------------
+// Différences entre une variable let et variable var
+
+//---BLOC1
+function varTest(){
+    // Déclaration + affectation d'une valeur à une variable créée
+    var x = 1;
+
+    // Condition
+    //---BLOC 2
+    if(true){
+        var x = 2; // C'est la même variable qui se trouve à l'extérieur de la condition (cf. 98)
+        console.log(x); // Affiche : 2
+    }
+    //---FIN DU BLOC 2
+
+    console.log(x); // Affiche : 2
+}
+//---FIN DU BLOC 1
+
+varTest(); // Appel de la fonction
+
+//-----------------
+
+//---BLOC1
+function letTest(){
+    // Déclaration + affectation d'une valeur à une variable créée
+    let x = 1;
+
+    // Condition
+    //---BLOC 2
+    if(true){
+        let x = 2; // C'est une variable différente
+        console.log(x); // Affiche : 2
+    }
+    //---FIN DU BLOC 2
+
+    console.log(x); // Affiche : 1
+}
+//---FIN DU BLOC 1
+
+letTest(); // Appel de la fonction
+// Une variable let est un espace de stockage permettant de stocker tout type de données DONT la portée est limitée à celle du bloc courant
+
+
+document.write("<h1>Les différents types de données</h1>");
+
+// STRING
+
+document.write("<h3>STRING (Chaine de caractères)</h3>");
+
+let str = "Hello world";
+let str2 = '2';
+let str3 = `3`;
+
+document.write(str + str2 ); // Affiche : Hello world 2
+
+let phraseApostrophe = 'JS, c\'est cool'; // L'antislash "\" indique à JS que l'apostrophe dans une chaine de caractère n'est pas la fermeture de ma chaine de caractères
+
+let phraseGuillemet = "Le formateur a dit : \"JavaScript, c'est génial\""; // L'antislash "\" indique à JS que les guillemet dans une chaine de caractère ne sont pas la fermeture de ma chaine de caractères
+
+//---------------------------
+// let prenom = prompt("Erivez votre prénom");
+
+// console.log(prenom);
+//---------------------------
+
+//---------------------------
+
+// NUMBER
+
+document.write("<h3>NUMBER (valeur numérique)</h3>");
+
+// Une valeur NUMBER se caractérise par l'absence de quote
+
+let chiffre = 20; // Valeur positive
+
+let negatif = -15; // Valeur négative
+
+let decimal = 2.5; // Valeur décimale caractérisée par un point "."
+
+document.write('Valeur positive : ' + chiffre + '<br>'); // Affiche : Valeur positive : 20
+
+document.write('Valeur négative : ' + negatif + '<br>'); // Affiche : Valeur négative : -15
+
+document.write('Valeur décimale : ' + decimal + '<br>'); // Affiche : Valeur décimal : 2.5
+
+
+//***** PARTICULARITE *********/
+
+let a = "36";
+let b = 67;
+
+document.write("Résultat entre un string et un number : " + a + b);
+
+//------------------------------
+// BOOLEAN (booléen)
+
+document.write("<h3>BOOLEAN (Booléen)</h3>");
+
+// Le type booléen ne prend en compte que 2 valeurs : true ou false
+
+let choix = true;
+let choix2 = false;
+
+document.write('Valeurs booléennes de mes variables choix et choix2 : ' + choix + " et " + choix2 + '<br>');
+
+//-------------------------------
+// OBJECTS
+
+document.write("<h3>OBJECTS</h3>");
+
+    // array (tableau)
+document.write("<h5>Array (tableau)</h5>");
+
+let animaux = ["chien", "chat", "lapin"];
+
+document.write(animaux + '<br>'); // chien, chat, lapin
+document.write(animaux[0] + '<br>'); // chien
+document.write(animaux[1] + '<br>'); // chat
+document.write(animaux[2] + '<br>'); // lapin
+
+    // object
+document.write("<h5>Object</h5>");
+
+let person = {
+    nom: "Dupont",
+    prenom: "Jean",
+    age: 58
+};
+
+document.write(person + '<br>'); // Affiche : [object Object]
+document.write(person.nom + '<br>'); // Affiche : Dupont
+document.write(person.prenom + '<br>'); // Affiche : Jean
+document.write(person.age + ' ans<br>'); // Affiche : 58 ans
+
+    // null
+let foo = null; // null équivaut à "rien"
+
+//------------------------------
+document.write("<h5>Undefined</h5>");
+    // UNDEFINED
+let maLet; // Simple déclaration de variable 
+
+//------------------------------
+
+document.write('<h3>typeof pour récupérer le type d\'une variable</h3>');
+
+document.write('Type de la variable "phraseApostrophe" : ' + typeof phraseApostrophe + '<br>'); // Type de la variable "phraseApostrophe" : string
+
+//***** EXERCICE *******/
+
+// Afficher, faisant des phrases, les types de données de chaque variable créée depuis la variable "phraseApostrophe" comme l'exemple de la ligne 231
+
+document.write('Type de la variable "phraseGuillemet" : ' + typeof phraseGuillemet + '<br>'); // Type de la variable "phraseGuillemet" : string
+
+document.write('Type de la variable "chiffre" : ' + typeof chiffre + '<br>'); // Type de la variable "chiffre" : number
+
+document.write('Type de la variable "negatif" : ' + typeof negatif + '<br>'); // Type de la variable "negatif" : number
+
+document.write('Type de la variable "decimal" : ' + typeof decimal + '<br>'); // Type de la variable "phraseApostrophe" : number
+
+document.write('Type de la variable "a" : ' + typeof a + '<br>'); // Type de la variable "a" : string
+
+document.write('Type de la variable "b" : ' + typeof pb + '<br>'); // Type de la variable "b" : undefined
+
+document.write('Type de la variable "choix" : ' + typeof choix + '<br>'); // Type de la variable "choix" : boolean
+
+document.write('Type de la variable "choix2" : ' + typeof choix2 + '<br>'); // Type de la variable "choix2" : boolean
+
+document.write('Type de la variable "animaux" : ' + typeof animaux + '<br>'); // Type de la variable "animaux" : object
+
+document.write('Type de la variable "person" : ' + typeof person + '<br>'); // Type de la variable "person" : object
+
+document.write('Type de la variable "foo" : ' + typeof foo + '<br>'); // Type de la variable "foo" : object
+
+document.write('Type de la variable "maLet" : ' + typeof maLet + '<br>'); // Type de la variable "maLet" : undefined
